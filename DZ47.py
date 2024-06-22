7ed3c5fa1fb729641939e75fb1e01caf59a7b22c
oryzhikh <oryzhikh@gmail.com>
2024-06-22 21:30:03 +0300

    1

--- DZ47.py
+++ DZ47.py
@@ -1,2 +1,39 @@
 
-print("Привет всем")
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.blog_catalog),
+    path('category/', views.category_list),
+    path('category/<int:category_id>/', views.category_detail),
+]
+
+# в основном файле urls.py вашего проекта
+from django.urls import include, path
+
+urlpatterns = [
+    # другие маршруты вашего проекта
+    path('blog/', include('blog.urls')),
+]
+
+# в файле views.py вашего приложения blog
+from django.http import HttpResponse, Http404
+
+categories = {
+    1: "чилл территории python",
+    2: "django, сложно, но можно!",
+    3: "flask, бегите, глупцы!",
+}
+
+def blog_catalog(request):
+    return HttpResponse("тут будет блог")
+
+def category_list(request):
+    categories_str = ", ".join([str(key) for key in categories.keys()])
+    return HttpResponse(categories_str)
+
+def category_detail(request, category_id):
+    if category_id in categories:
+        return HttpResponse(categories[category_id])
+    else:
+        raise Http404
